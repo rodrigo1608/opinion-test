d5ed0852e4870ed70a83f4cc67fae5a90d5d1243
rolmo <rodrigolmo.dev@gmail.com>
2025-08-29 03:12:25 -0300

    refactor: Refact dadhboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,11 +8,13 @@ use App\Models\User;
 class DashboardController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Display a listing of the users on the dashboard.
      */
-    public function __invoke(Request $request)
+    public function index(Request $request)
     {
         $users = User::all();
-        return view('dashboard',compact('users'));
+        return view('dashboard', compact('users'));
     }
+
+    // Você pode adicionar outros métodos aqui, como 'show', 'edit', etc.
 }
